e3cc9e7dd1cd6e10d0ff60a9c43f8f08254accb5
Kaiwei1011 <43839923+Kaiwei1011@users.noreply.github.com>
2018-10-11 16:14:25 +0800

    Add files via upload

---
+++ 1011/prog4_3.c
@@ -0,0 +1,12 @@
+/* prog4_3, 使用printf()函數 */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+   int num=25;
+   printf("\"C:\\\\happyC\"\n",num);    /* 印出字串 */
+
+   system("pause");
+   return 0;
+}
+
